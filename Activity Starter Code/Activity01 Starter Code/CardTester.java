f499f83e35f7b04c311a2e7b7ba5e8a2b6f3d58e
Brendan Haines <brendanhaines@users.noreply.github.com>
2015-04-02 14:32:47 -0600

    Update CardTester.java

--- Activity Starter Code/Activity01 Starter Code/CardTester.java
+++ Activity Starter Code/Activity01 Starter Code/CardTester.java
@@ -13,16 +13,41 @@ public class CardTester {
 		Card c = new Card("10","Jasons",40);
 		Card d = new Card("Jack","Evans",40);
 
+		System.out.println( "EXPECTED RESULTS:" );
+		System.out.println( "5 of Kings (point value = 40)" );
+		System.out.println( "10 of Jasons (point value = 40)" );
+		System.out.println( "10 of Jasons (point value = 40)" );
+		System.out.println( "Jack of Evans (point value = 40)" );
+
+		System.out.println( "testing toString()..." );
 		System.out.println(a);
 		System.out.println(b);
 		System.out.println(c);
 		System.out.println(d);
 
+		System.out.println( "testing matches(card)..." );
 		System.out.println(a.matches(b));
 		System.out.println(b.matches(c));
 		System.out.println(c.matches(d));
 		System.out.println(d.matches(a));
+		
+		System.out.println( "testing suit()..." );
+		System.out.println( a.suit() );
+		System.out.println( b.suit() );
+		System.out.println( c.suit() );
+		System.out.println( d.suit() );
 
-
+		System.out.println( "testing rank()..." );
+		System.out.println( a.rank() );
+		System.out.println( b.rank() );
+		System.out.println( c.rank() );
+		System.out.println( d.rank() );
+		
+		System.out.println( "testing pointValue()..." );
+		System.out.println( a.pointValue() );
+		System.out.println( b.pointValue() );
+		System.out.println( c.pointValue() );
+		System.out.println( d.pointValue() );
+		
 	}
 }
