927143e9101785c5c61c2db72438d61fe31a1af9
Will Kaufman <wjkhoops@gmail.com>
2015-04-02 15:15:41 -0600

    updates DeckTester to include 0-length decks

--- Activity Starter Code/Activity02  Starter Code/DeckTester.java
+++ Activity Starter Code/Activity02  Starter Code/DeckTester.java
@@ -20,31 +20,31 @@ public class DeckTester {
 
 		Deck bDeck = new Deck(bRank, bSuit, bVals);
 
-		// String[] cRank = new String[1];
-		// String[] cSuit = new String[1];
-		// int[] cVals = new int[1];
-		//
-		// Deck cDeck = new Deck(cRank, cSuit, cVals);
+		String[] cRank = new String[0];
+		String[] cSuit = new String[0];
+		int[] cVals = new int[0];
+
+		Deck cDeck = new Deck(cRank, cSuit, cVals);
 
 		System.out.println(aDeck.isEmpty());
 		System.out.println(bDeck.isEmpty());
-		// System.out.println(cDeck.isEmpty());
+		System.out.println(cDeck.isEmpty());
 
 		System.out.println(aDeck.size());
 		System.out.println(bDeck.size());
-		// System.out.println(cDeck.size());
+		System.out.println(cDeck.size());
 
 		System.out.println(aDeck);
 		System.out.println(bDeck);
-		// System.out.println(cDeck);
+		System.out.println(cDeck);
 
 		System.out.println(aDeck.deal());
 		System.out.println(bDeck.deal());
-		// System.out.println(cDeck.deal());
+		System.out.println(cDeck.deal());
 
 		System.out.println(aDeck);
 		System.out.println(bDeck);
-		// System.out.println(cDeck);
+		System.out.println(cDeck);
 
 
 
